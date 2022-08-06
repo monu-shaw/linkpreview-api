a442346aecfbc0650caa18cc3e44508f495bfc56
monu-shaw <premraj00071@gmail.com>
2022-08-06 05:31:25 +0530

    bug resolved

--- pages/api/link.js
+++ pages/api/link.js
@@ -6,7 +6,7 @@ export default function handler(req, res) {
     //res.status(200).json({ name:  })
     if(req.method  === 'POST'){
         try {
-            getLinkPreview(req.body.ur).then(re=>res.json({status: 1,data: re})).catch((er)=>res.json({status: 0,data: req.body}));      
+            getLinkPreview(req.body.url).then(re=>res.json({status: 1,data: re})).catch((er)=>res.json({status: 0,data: req.body}));      
             //res.json({"data": req.body.id})
         } catch (error) {
             res.send(`error ${error}`)
