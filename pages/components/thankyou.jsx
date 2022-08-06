ae588002c41385ca45bc5d690864a354e6eae780
monu-shaw <premraj00071@gmail.com>
2022-08-06 23:23:39 +0530

    Final Coomit

--- pages/components/thankyou.jsx
+++ pages/components/thankyou.jsx
@@ -14,7 +14,7 @@ export function Thankyou(){
     
         <ul>
           <li>Next Js Crash Course <a href="https://courses.learncodeonline.in/learn/NextJS-Full-stack-crash-course" target="_blank" rel="noreferrer">Hitesh Chowdhary(LCO)</a></li>
-          <li>Web Scraping with Axios &#x26; Cherio <a href="https://dev.to/drsimplegraffiti/i-scraped-dev-to-using-axios-and-cheerio-26ko" target="_blank" rel="noreferrer">Abayomi Ogunnusi(Dev.to)</a></li>
+          <li>Web Scraping with Axios and Cherio <a href="https://dev.to/drsimplegraffiti/i-scraped-dev-to-using-axios-and-cheerio-26ko" target="_blank" rel="noreferrer">Abayomi Ogunnusi(Dev.to)</a></li>
           <li>Logic for getting individual elements for link preview <a href="https://andrejgajdos.com/how-to-create-a-link-preview" target="_blank" rel="noreferrer">Andrej Gajdos(andrejgajdos.com)</a></li>
         </ul>
   )
