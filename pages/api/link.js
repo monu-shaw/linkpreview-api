3c56482ff191ab4ddcb08e38b467b2d894c25e54
Monu Shaw <premraj00071@gmail.com>
2022-12-30 09:04:55 +0000

    "Cors Added"

--- pages/api/link.js
+++ pages/api/link.js
@@ -1,9 +1,15 @@
 import getLinkPreview from 'monu-linkpreview';
+import NextCors from 'nextjs-cors';
 
 
 export default async function handler(req, res) {
   
-    //res.status(200).json({ name:  })
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+     });
     if(req.method  === 'POST'){
         try {
              await getLinkPreview(req.body.url).then(re=>res.json({status: 1,data: re})).catch((er)=>res.json({status: 0,data: er}));      
